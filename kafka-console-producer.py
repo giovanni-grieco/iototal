bfab84914b960005faae9a4a342ed68ff21b3b67
giovanni <giovi.5057@gmail.com>
2025-05-03 20:08:10 +0200

    beautified input

--- kafka-console-producer.py
+++ kafka-console-producer.py
@@ -9,9 +9,7 @@ try:
     producer = KafkaProducer(bootstrap_servers=f"{bootstrap_server}:9094")
 
     while True:
-        message = input("Enter message to send to Kafka topic (or 'exit' to quit): ")
-        if message.lower() == 'exit':
-            break
+        message = input("Enter message to send: ")
         producer.send('test-topic', value=message.encode('utf-8'))
         print(f"Sent: {message}")
 except Exception as e:
