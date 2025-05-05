6ea7a0b852730cd12425133cce0e3d72588d6944
giovanni <giovi.5057@gmail.com>
2025-05-05 11:25:29 +0200

    migliorato stop-cluster

--- stop-cluster.sh
+++ stop-cluster.sh
@@ -1,5 +1,7 @@
 helm uninstall iototal
 
+kubectl delete clusterrolebinding spark-role
+kubectl delete serviceaccount spark
 kubectl delete pods --all
 kubectl delete pvc --all
 kubectl delete pv --all
